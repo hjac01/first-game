8041f2a91593b9106524f0ecc96bcaa71e0b94ad
Hannah Jacoby <hjacoby@grubhub.com>
2019-07-10 17:04:06 -0600

    js edits

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// VARIABLES
+var wins = 0;
+var losses = 0;
+var randomNumber = [];
+
+// FUNCTIONS
+
+$(document).ready(function() {
+    $('#random-button').on('click', function() {
+var min=19; 
+var max=120;
+var random = Math.floor (Math.random() * (+max - +min)) + +min; 
+
+    $('#random-number').prepend();
+});
+
+});
+
+    
+
+// // BUTTONS
+
